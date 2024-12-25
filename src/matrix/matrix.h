d6e31b6b7a2d9f78c09bec2f30d2ee444303a343
Ferhat HAMEL <ferhat.hamel@univ-rouen.fr>
2024-12-25 15:21:27 +0100

    MATRIX : matrix module v0.0.1.

--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -5,8 +5,14 @@
 
 typedef struct matrix matrix_t;
 
+int matrix_truncate(int fd, size_t nbLines, size_t NbCols, size_t *sizePtr);
+int matrix_init(matrix_t *m, size_t nbLines, size_t nbColumns);
+
 size_t matrix_size(matrix_t *m);
-void matrix_cell_set_random(matrix_t *m, size_t ligne, size_t column);
+int matrix_cell_set_random(matrix_t *m, size_t line, size_t column);
 int matrix_write(int fd, matrix_t *m);
+
+int matrix_get_lock(matrix_t *m);
+int matrix_release_lock(matrix_t *m);
 
 #endif
