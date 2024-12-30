9a4c622080b316261dee2954d35c1a8224b65bf0
Ferhat HAMEL <ferhat.hamel@univ-rouen.fr>
2024-12-30 16:40:09 +0100

    updated get_pid_based_name.(cherry-pick from master)

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,9 +1,10 @@
 #include "utils.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <sys/types.h>
 
-char *get_pid_based_name() {
-  int pid = (int) getpid();
+char *get_pid_based_name(pid_t __pid) {
+  int pid = (int) __pid;
   size_t strSize = (size_t) snprintf(NULL, 0, "%d", pid);
   char *str = malloc(strSize + 1);
   snprintf(str, strSize, "%d", pid);
