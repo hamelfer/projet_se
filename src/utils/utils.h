e86cda7d7be79f6f9569831d71987cd227494891
Ferhat HAMEL <ferhat.hamel@univ-rouen.fr>
2024-12-30 16:37:26 +0100

    updated get_pid_based_name.

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,10 +1,12 @@
 #ifndef UTILS__H
 #define UTILS__H
 
+#include <sys/types.h>
+
 //  Renvoie une chaine de caracteres stockee dans le tas
 //    basee sur le pid de l'appelant.
 //  Un appel a free avec comme argument l'adresse retournee
 //    peut etre envisage.
-char *get_pid_based_name();
+char *get_pid_based_name(pid_t pid);
 
 #endif
