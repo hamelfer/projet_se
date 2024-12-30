4dc5b702188087265bd53b07f89b58dca2210a96
Ferhat HAMEL <ferhat.hamel@univ-rouen.fr>
2024-12-30 19:16:20 +0100

    Add specifications.

--- src/workera/workera.h
+++ src/workera/workera.h
@@ -4,8 +4,12 @@
 #include "segment.h"
 #include <stddef.h>
 
-// fonction qui fait le travail de workera du projet et cree un processus enfant
-//   qui va gerer le travail de workerb
+// Fonction qui cree un segment et initialise la matrice A avec des
+//    valeurs aleatoires ne depassant pas maxInt et qui lance un
+//    processus de type workerb, puis renvoie le le segment une fois
+//    le processus cree se termine.
+// En cas de succes renvoie un pointeur vers le segment.
+// Renvoie sinon NULL.
 segment_t *worker_a(size_t n, size_t m, size_t p, int maxInt);
 
 #endif
--- src/workerb/workerb.h
+++ src/workerb/workerb.h
@@ -3,7 +3,12 @@
 
 #include "segment.h"
 
-// fonction qui fait le travail de workerb du projet
+// Fonction aui initialise la matrice B su segment pointee par s
+//    avec des valeurs aleatoires ne depassant pas maxInt et qui calcule
+//    la matrice C qui est le produit de la matrice A et B en
+//    utilisant ((nombre de ligne de C) * (nombre de colonnes de C))
+//    threads.
+//  Renvoie 0 en cas de succes. Renvoie sinon -1.
 int worker_b(segment_t *s, int maxInt);
 
 #endif
