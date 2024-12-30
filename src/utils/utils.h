7de0631cf2069ae5289787d87a5da66d07886775
Ferhat HAMEL <ferhat.hamel@univ-rouen.fr>
2024-12-30 13:19:27 +0100

    Add specification to utils function in utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,10 @@
 #ifndef UTILS__H
 #define UTILS__H
 
+//  Renvoie une chaine de caracteres stockee dans le tas
+//    basee sur le pid de l'appelant.
+//  Un appel a free avec comme argument l'adresse retournee
+//    peut etre envisage.
 char *get_pid_based_name();
 
 #endif
